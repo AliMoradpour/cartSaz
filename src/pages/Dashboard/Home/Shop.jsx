21e602268ffabe0115f46268dd2853a8f1bb8148
AliMoradpour <mr.mrdp@yahoo.com>
2023-06-06 15:13:22 +0430

    fix  search product logic

--- src/pages/Dashboard/Home/Shop.jsx
+++ src/pages/Dashboard/Home/Shop.jsx
@@ -6,21 +6,30 @@ import ShopContact from "./ShopContact";
 import ShopSearch from "./ShopSearch";
 import { products } from "/src/constant/data.js";
 
-
 const Shop = () => {
   const [isClicked, setIsClicked] = useState("products");
-  const [filteredProducts, setFilteredProducts] = useState([])
-
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   return (
     <>
       <ShopHeader />
       <ShopSort setIsClicked={setIsClicked} isClicked={isClicked} />
-      <ShopSearch setFilteredProducts={setFilteredProducts} products={products} />
+      <ShopSearch
+        setFilteredProducts={setFilteredProducts}
+        products={products}
+      />
       <div className="flex">
         <div className="w-1/4 h-screen hidden xl:block"></div>
         <div className="xl:w-3/4 xl:-mr-36 mt-6">
-          {isClicked == "products" ? <ProductList filteredProducts={filteredProducts}/> : <ShopContact />}
+          {isClicked == "products" ? (
+            <ProductList
+              filteredProducts={
+                filteredProducts.length > 0 ? filteredProducts : products
+              }
+            />
+          ) : (
+            <ShopContact />
+          )}
         </div>
       </div>
     </>
