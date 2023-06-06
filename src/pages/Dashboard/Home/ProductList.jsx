3a01b650a240b03f71974dde275478049c103818
AliMoradpour <mr.mrdp@yahoo.com>
2023-06-06 15:22:33 +0430

    fix some logic in  product list

--- src/pages/Dashboard/Home/ProductList.jsx
+++ src/pages/Dashboard/Home/ProductList.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 const ProductList = ({filteredProducts}) => {
   
   return (
-    <div className="w-full flex flex-wrap gap-4">
+    <div className="w-full flex flex-wrap justify-center md:justify-start gap-4 mb-20 px-10">
       {filteredProducts.map((item) => (
         <Product item={item} key={item.id} />
       ))}
